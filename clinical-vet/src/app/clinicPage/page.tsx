f04672f309fd5c72c9b1e9a9626eb8c712c007be
YuriDonato <yuridonatosis@gmail.com>
2023-07-11 15:21:56 -0300

    Update page.tsx

--- clinical-vet/src/app/clinicPage/page.tsx
+++ clinical-vet/src/app/clinicPage/page.tsx
@@ -61,7 +61,7 @@ export default function Clinic() {
         <div className="grid grid-cols-6 gap-3 pl-10 pr-10 pt-10">
           <div className="bg-azulclaro text-white shadow-lg rounded-lg p-6">
             <h2 className="text-xl font-bold mb-4">Nome da Doença</h2>
-            <p className="text-gray-600 mb-4">
+            <p className="text-gray-100 mb-4">
               Descrição da doença Lorem ipsum dolor sit amet, consectetur
               adipiscing elit.
             </p>
