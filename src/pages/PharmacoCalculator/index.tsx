4f32685956ba783e075a963e5bfb4bc9adbed9ca
Yuri Donato <yuridonatosis@gmail.com>
2024-09-02 22:10:13 -0300

    Update index.tsx

--- src/pages/PharmacoCalculator/index.tsx
+++ src/pages/PharmacoCalculator/index.tsx
@@ -3,6 +3,7 @@ import {
   FormLabel,
   Input,
   Select,
+  Switch,
   Table,
   TableContainer,
   Tbody,
@@ -24,17 +25,37 @@ const PharmacoCalculator = () => {
   const [concentracao, setConcentracao] = useState('')
 
   const [resultadoCalculo, setResultadoCalculo] = useState('')
+
+  const [isComprimido, setIsComprimido] = useState(false)
   const isMobile = useBreakpoint()
 
   const Calcular = () => {
-    const tempVolume = parseInt(volume)
-    const tempPeso = parseInt(peso)
-    const tempDose = parseInt(dose)
-    const tempConcentracao = parseInt(concentracao)
+    const tempVolume = parseFloat(volume.replace(',', '.'))
+    const tempPeso = parseFloat(peso.replace(',', '.'))
+    const tempDose = parseFloat(dose.replace(',', '.'))
+    const tempConcentracao = parseFloat(concentracao.replace(',', '.'))
+    let tempResultado = 0
+    console.log(tempDose, tempPeso)
+    if (isComprimido) {
+      tempResultado = tempPeso * tempDose
+    } else {
+      tempResultado = (tempPeso * tempDose) / tempConcentracao
+    }
 
-    const tempResultado = (tempPeso * tempDose) / tempConcentracao
     setResultadoCalculo(tempResultado.toString())
   }
+
+  const ClearValues = () => {
+    setVolume('')
+    setPeso('')
+    setDose('')
+    setConcentracao('')
+    setResultadoCalculo('')
+  }
+
+  useEffect(() => {
+    ClearValues()
+  }, [isComprimido])
 
   useEffect(() => {
     Calcular()
@@ -42,41 +63,86 @@ const PharmacoCalculator = () => {
   return (
     <>
       <Container>
-        <h1>Calculadora</h1>
-
-        {/* Peso do animal */}
-        <FormLabel>Peso</FormLabel>
-        <Input
-          placeholder="Insira o peso do animal"
-          value={peso}
-          onChange={(e) => {
-            setPeso(e.target.value)
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '100%'
           }}
-        ></Input>
+        >
+          <h1>Calculadora</h1>
+          <div
+            style={{
+              display: 'flex'
+            }}
+          >
+            <h2>{isComprimido ? 'Comprimido' : 'Solução'}</h2>
+            <Switch onChange={() => setIsComprimido(!isComprimido)}></Switch>
+          </div>
+        </div>
+        {isComprimido ? (
+          <>
+            {/* Peso do animal */}
+            <FormLabel>Peso</FormLabel>
+            <Input
+              placeholder="Insira o peso do animal"
+              value={peso}
+              onChange={(e) => {
+                setPeso(e.target.value)
+              }}
+            ></Input>
+            {/* Dose */}
+            <FormLabel>Dose</FormLabel>
+            <Input
+              placeholder="Insira a dose"
+              value={dose}
+              onChange={(e) => {
+                setDose(e.target.value)
+              }}
+            ></Input>
+            {/* somar tudo e mostrar */}
+            <h1>
+              {isComprimido ? 'Miligramas' : 'Volume'}: {resultadoCalculo}
+            </h1>{' '}
+          </>
+        ) : (
+          <>
+            {/* Peso do animal */}
+            <FormLabel>Peso</FormLabel>
+            <Input
+              placeholder="Insira o peso do animal"
+              value={peso}
+              onChange={(e) => {
+                setPeso(e.target.value)
+              }}
+            ></Input>
 
-        {/* Dose */}
-        <FormLabel>Dose</FormLabel>
-        <Input
-          placeholder="Insira a dose"
-          value={dose}
-          onChange={(e) => {
-            setDose(e.target.value)
-          }}
-        ></Input>
+            {/* Dose */}
+            <FormLabel>Dose</FormLabel>
+            <Input
+              placeholder="Insira a dose"
+              value={dose}
+              onChange={(e) => {
+                setDose(e.target.value)
+              }}
+            ></Input>
 
-        {/* Concentracao */}
-        <FormLabel>Concentração</FormLabel>
-        <Input
-          placeholder="Insira a concentração"
-          value={concentracao}
-          onChange={(e) => {
-            setConcentracao(e.target.value)
-          }}
-        ></Input>
+            {/* Concentracao */}
+            <FormLabel>Concentração</FormLabel>
+            <Input
+              placeholder="Insira a concentração"
+              value={concentracao}
+              onChange={(e) => {
+                setConcentracao(e.target.value)
+              }}
+            ></Input>
 
-        {/* somar tudo e mostrar */}
-        <h1>Volume: {resultadoCalculo}ml</h1>
-        {/* <h1>Velocidade de Infusão: {taxaInfusao}ml/hora</h1> */}
+            {/* somar tudo e mostrar */}
+            <h1>
+              {isComprimido ? 'Miligramas' : 'Volume'}: {resultadoCalculo}
+            </h1>
+          </>
+        )}
       </Container>
     </>
   )
